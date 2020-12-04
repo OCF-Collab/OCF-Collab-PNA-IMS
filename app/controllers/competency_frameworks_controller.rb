0040baeddb28030942bed140a5645118a7d11342
Tomasz Werbicki <tomasz@werbicki.net>
2020-12-04 13:55:43 +0100

    Accept id in query instead of path parameters

--- app/controllers/competency_frameworks_controller.rb
+++ app/controllers/competency_frameworks_controller.rb
@@ -1,6 +1,6 @@
 class CompetencyFrameworksController < ApplicationController
-  def show
-    fetcher = CompetencyFrameworkFetcher.new(id: params[:id])
+  def asset_file
+    fetcher = CompetencyFrameworkFetcher.new(id: params.require(:id))
 
     send_data fetcher.body, type: fetcher.content_type, disposition: :inline
   end
